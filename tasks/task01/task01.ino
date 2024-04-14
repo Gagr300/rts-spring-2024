d8599d5ef45970c36147a4c1917568727559a00b
Gromova Galina <71606935+Gagr300@users.noreply.github.com>
2024-04-14 16:56:03 +0400

    Update task01

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -17,19 +17,68 @@ void setup() {
 void loop() {
   unsigned long currentMicros = micros();
 
-  for (int i = 0; i < PIN_NUMBER; i++) {
-    if (currentMicros - prevMicros[i] >= BLINK_INTERVALs[i]) {
-      prevMicros[i] = currentMicros;
+ if (currentMicros - prevMicros[i] >= BLINK_INTERVALs[0]) {
+    prevMicros[0] = currentMicros;
 
-      if (ledStates[i] == LOW) {
-          ledStates[i] = HIGH;
-      }
-      else {
-          ledStates[i] = LOW;
-      }
-
-      digitalWrite(LED_PINs[i], ledStates[i]);
+    if (ledStates[0] == LOW) {
+        ledStates[0] = HIGH;
     }
-  }
+    else {
+        ledStates[0] = LOW;
+    }
 
+    digitalWrite(LED_PINs[0], ledStates[0]);
+  }
+  
+  if (currentMicros - prevMicros[1] >= BLINK_INTERVALs[1]) {
+    prevMicros[1] = currentMicros;
+
+    if (ledStates[1] == LOW) {
+        ledStates[1] = HIGH;
+    }
+    else {
+        ledStates[1] = LOW;
+    }
+
+    digitalWrite(LED_PINs[1], ledStates[1]);
+  }
+  
+  if (currentMicros - prevMicros[2] >= BLINK_INTERVALs[2]) {
+    prevMicros[2] = currentMicros;
+
+    if (ledStates[2] == LOW) {
+        ledStates[2] = HIGH;
+    }
+    else {
+        ledStates[2] = LOW;
+    }
+
+    digitalWrite(LED_PINs[2], ledStates[2]);
+  }
+  
+  if (currentMicros - prevMicros[3] >= BLINK_INTERVALs[3]) {
+    prevMicros[3] = currentMicros;
+
+    if (ledStates[3] == LOW) {
+        ledStates[3] = HIGH;
+    }
+    else {
+        ledStates[3] = LOW;
+    }
+
+    digitalWrite(LED_PINs[3], ledStates[3]);
+  }
+  
+  if (currentMicros - prevMicros[4] >= BLINK_INTERVALs[4]) {
+    prevMicros[4] = currentMicros;
+
+    if (ledStates[4] == LOW) {
+        ledStates[4] = HIGH;
+    }
+    else {
+        ledStates[4] = LOW;
+    }
+
+    digitalWrite(LED_PINs[4], ledStates[4]);
+  }
 }
